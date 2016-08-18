da9bb218448a5ae42e984b05344ab8edc1a60111
Andrew Stuart <andrew.stuart2@gmail.com>
2016-08-18 12:59:40 -0700

    Re-add command so that binary gets run

--- main.go
+++ main.go
@@ -139,6 +139,7 @@ func main() {
 
 func (ctr *certer) addTLSSecrets(ing *extensions.Ingress) (*extensions.Ingress, error) {
 	if len(ing.Spec.TLS) == 0 && !*forceTLS {
+		log.Println(*forceTLS)
 		return nil, fmt.Errorf("No ingresses to update")
 	}
 
@@ -212,34 +213,38 @@ func (ctr *certer) addTLSSecrets(ing *extensions.Ingress) (*extensions.Ingress, 
 }
 
 func (ctr *certer) watchIng() {
-	w, err := ctr.api.Extensions().Ingress("").Watch(api.ListOptions{})
-	if err != nil {
-		log.Fatal("Watch error", err)
-		return
-	}
-
-	for evt := range w.ResultChan() {
-		et := watch.EventType(evt.Type)
-		if et != watch.Added && et != watch.Modified {
-			continue
-		}
-
-		originalObjJS, err := runtime.Encode(api.Codecs.LegacyCodec(), evt.Object)
+	for {
+		w, err := ctr.api.Extensions().Ingress("").Watch(api.ListOptions{})
 		if err != nil {
-			log.Println("Object decode error", err)
-			continue
+			log.Fatal("Watch error", err)
+			return
 		}
 
-		i := &extensions.Ingress{}
-		err = json.Unmarshal(originalObjJS, i)
-		if err != nil {
-			log.Println("Ingress Unmarshal error", err)
-			continue
+		for evt := range w.ResultChan() {
+			et := watch.EventType(evt.Type)
+			if et != watch.Added && et != watch.Modified {
+				continue
+			}
+
+			originalObjJS, err := runtime.Encode(api.Codecs.LegacyCodec(), evt.Object)
+			if err != nil {
+				log.Println("Object decode error", err)
+				continue
+			}
+
+			i := &extensions.Ingress{}
+			err = json.Unmarshal(originalObjJS, i)
+			if err != nil {
+				log.Println("Ingress Unmarshal error", err)
+				continue
+			}
+
+			_, err = ctr.addTLSSecrets(i)
+			if err != nil {
+				log.Printf("Error adding secret for new/updated ingress: %s/%s: %s", i.Namespace, i.Name, err)
+			}
 		}
 
-		_, err = ctr.addTLSSecrets(i)
-		if err != nil {
-			log.Printf("Error adding secret for new/updated ingress: %s/%s: %s", i.Namespace, i.Name, err)
-		}
+		log.Println("Result channel closed. Starting again.")
 	}
 }
